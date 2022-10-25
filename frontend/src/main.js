17ce824942b9d256f79062e31aafc717fc3f45e5
IBSHAMI <alshamiibrahim96@gmail.com>
2022-10-25 22:19:23 +0800

    feat:
    1. deleted the default counter state.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -8,7 +8,11 @@ import "./assets/base.css";
 
 const app = createApp(App);
 
+// we register the plugin on the app
+// before creating the router
 app.use(createPinia());
+
+// we create the router after the plugin
 app.use(router);
 
 app.mount("#app");
