b08a5564576ef852077199a9a44c89b9fe6ae387
IBSHAMI <alshamiibrahim96@gmail.com>
2022-10-25 20:53:11 +0800

    feat:
    1. Configured the vue router and added the home page (landing page).
    
    2. downloaded tailwindcss and its libraries to the project.

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,23 +1,19 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+// Vue excpect this to be an array of objects with a path and a component
+// each object is called a route record
+const routes = [
+  {
+    path: "/", // the path to match
+    name: "LandingPage", // the name of the route
+    component: HomeView, // the component to render when the route is matched
+  },
+];
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView
-    },
-    {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
-  ]
-})
+  routes,
+});
 
-export default router
+export default router;
