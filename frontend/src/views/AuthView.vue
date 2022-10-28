29571a0f501f3561ab3191fcbcbc24e8c0b5945e
IBSHAMI <alshamiibrahim96@gmail.com>
2022-10-28 18:51:03 +0800

    feat:
    1. Added username field o signup form.

--- frontend/src/views/AuthView.vue
+++ frontend/src/views/AuthView.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <main class="container">
+      <!-- Sign in-->
       <section class="absolute w-full h-full bg-[#6b7280]" v-if="!signup">
         <div>
           <div
@@ -105,6 +106,7 @@
           </div>
         </div>
       </section>
+      <!-- Sign up -->
       <section class="bg-[#6b7280]" v-else-if="signup">
         <div
           class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0"
@@ -130,6 +132,21 @@
                 Create an account
               </h1>
               <form class="space-y-4 md:space-y-6" action="#">
+                <div>
+                  <label
+                    for="email"
+                    class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                    >Username</label
+                  >
+                  <input
+                    type="text"
+                    name="username"
+                    id="username"
+                    class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                    placeholder="username"
+                    required=""
+                  />
+                </div>
                 <div>
                   <label
                     for="email"
