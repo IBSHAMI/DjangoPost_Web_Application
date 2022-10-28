045581aaf6dfd382b3ffbc165c3987daa782df4c
IBSHAMI <alshamiibrahim96@gmail.com>
2022-10-28 20:43:24 +0800

    feat:
    1. Changed the form and input to the imported vee-form and vee-field.
    
    2. Added validation to all sign up fields.

--- frontend/src/includes/validation.js
+++ frontend/src/includes/validation.js
@@ -1,4 +1,17 @@
-import { Form as VeeForm, Field as VeeField } from "vee-validate";
+import {
+  Form as VeeForm,
+  Field as VeeField,
+  defineRule,
+  ErrorMessage,
+} from "vee-validate";
+import {
+  required,
+  min,
+  max,
+  alpha_spaces as alphaSpaces,
+  email,
+  confirmed,
+} from "@vee-validate/rules";
 
 // register a custom plugin,
 // Vue will run the install function before mounting the app
@@ -10,5 +23,13 @@ export default {
   install(app) {
     app.component("VeeForm", VeeForm);
     app.component("VeeField", VeeField);
+    app.component("ErrorMessage", ErrorMessage);
+
+    defineRule("required", required);
+    defineRule("min", min);
+    defineRule("max", max);
+    defineRule("alpha_spaces", alphaSpaces);
+    defineRule("email", email);
+    defineRule("confirmed", confirmed);
   },
 };
--- frontend/src/views/AuthView.vue
+++ frontend/src/views/AuthView.vue
@@ -131,20 +131,27 @@
               >
                 Create an account
               </h1>
-              <form class="space-y-4 md:space-y-6" action="#">
+              <vee-form
+                class="space-y-4 md:space-y-6"
+                action="#"
+                :validation-schema="schema"
+              >
                 <div>
                   <label
                     for="email"
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                     >Username</label
                   >
-                  <input
+                  <vee-field
                     type="text"
                     name="username"
                     id="username"
                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="username"
-                    required=""
+                  />
+                  <ErrorMessage
+                    name="username"
+                    class="text-red-500 text-xs italic"
                   />
                 </div>
                 <div>
@@ -153,13 +160,17 @@
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                     >Your email</label
                   >
-                  <input
+                  <vee-field
                     type="email"
                     name="email"
                     id="email"
                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     placeholder="name@company.com"
                     required=""
+                  />
+                  <ErrorMessage
+                    name="email"
+                    class="text-red-500 text-xs italic"
                   />
                 </div>
                 <div>
@@ -168,7 +179,7 @@
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                     >Password</label
                   >
-                  <input
+                  <vee-field
                     type="password"
                     name="password"
                     id="password"
@@ -176,30 +187,39 @@
                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required=""
                   />
+                  <ErrorMessage
+                    name="password"
+                    class="text-red-500 text-xs italic"
+                  />
                 </div>
                 <div>
                   <label
-                    for="confirm-password"
+                    for="confirm_password"
                     class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
                     >Confirm password</label
                   >
-                  <input
-                    type="confirm-password"
-                    name="confirm-password"
-                    id="confirm-password"
+                  <vee-field
+                    type="confirm_password"
+                    name="confirm_password"
+                    id="confirm_password"
                     placeholder="••••••••"
                     class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                     required=""
                   />
+                  <ErrorMessage
+                    name="confirm_password"
+                    class="text-red-500 text-xs italic"
+                  />
                 </div>
                 <div class="flex items-start">
                   <div class="flex items-center h-5">
-                    <input
+                    <vee-field
                       id="terms"
                       aria-describedby="terms"
                       type="checkbox"
                       class="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
-                      required=""
+                      name="terms"
+                      value="1"
                     />
                   </div>
                   <div class="ml-3 text-sm">
@@ -214,6 +234,12 @@
                       ></label
                     >
                   </div>
+                </div>
+                <div>
+                  <ErrorMessage
+                    name="terms"
+                    class="text-red-500 text-xs italic"
+                  />
                 </div>
                 <button
                   type="submit"
@@ -230,7 +256,7 @@
                     >Login here</a
                   >
                 </p>
-              </form>
+              </vee-form>
             </div>
           </div>
         </div>
@@ -249,6 +275,13 @@ export default {
   data() {
     return {
       signup: false,
+      schema: {
+        username: "required|min:3|max:20|alpha_spaces",
+        email: "required|min:3|max:100|email",
+        password: "required|min:3|max:100",
+        confirm_password: "confirmed:@password",
+        terms: "required|",
+      },
     };
   },
   methods: {
