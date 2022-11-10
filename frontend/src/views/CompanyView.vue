d3471d5c4f8e232a8408598e0ff9d17332c22793
IBSHAMI <alshamiibrahim96@gmail.com>
2022-11-10 19:24:24 +0800

    feat:
    1. Added company profile data vue.
    
    2. Added the initial boiler plate for the component.

--- frontend/src/views/CompanyView.vue
+++ frontend/src/views/CompanyView.vue
@@ -1,21 +1,28 @@
 <template>
-  <div>
-    <main-navbar-item :companyProfile="companyProfile" />
+  <div class="relative bg-white mx-auto max-w-7xl px-4 sm:px-6">
+    <main-navbar-item class="pb-8" :companyProfile="companyProfile" />
+    <company-data-item v-if="passedSlug === 'data'" />
   </div>
 </template>
 
 <script>
 import MainNavbarItem from "@/components/MainNavbarItem.vue";
+import CompanyDataItem from "../components/CompanyDataItem.vue";
 
 export default {
   name: "CompanyView",
   data() {
     return {
       companyProfile: true,
+      passedSlug: this.$route.params.slug,
     };
   },
   components: {
     MainNavbarItem,
+    CompanyDataItem,
+  },
+  $route(to) {
+    this.passedSlug = to.params.slug;
   },
 };
 </script>
