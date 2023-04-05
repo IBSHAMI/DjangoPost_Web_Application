ba8aa96e849ab529f874e3f55b3d7d37ebef4e1d
IBSHAMI <alshamiibrahim96@gmail.com>
2023-04-06 01:27:16 +0800

    backend deployment:
    1. Edited runserver and settings.py to include create super user.

--- backend/runserver.sh
+++ backend/runserver.sh
@@ -1,13 +1,23 @@
 #!/bin/sh
 
 # Collect static files
+echo "Collecting static files..."
 python manage.py collectstatic --noinput
 
+# Make database migrations
+echo "Making database migrations..."
+python manage.py makemigrations
+
+
 # Apply database migrations
+echo "Applying database migrations..."
 python manage.py migrate
 
 # create superuser
-python manage.py createsuperuser --noinput
+echo "Creating superuser..."
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.create_superuser(username='$DJANGO_SUPERUSER_USERNAME', email='$DJANGO_SUPERUSER_EMAIL', password='$DJANGO_SUPERUSER_PASSWORD')"
+
 
 # Start server using gunicorn
+echo "Starting server..."
 gunicorn DjangoPost.wsgi --bind=0.0.0.0:80
